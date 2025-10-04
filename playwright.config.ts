3b2fae3ac14591b5654e8f486c08bf3619f3af5c
Elv500 <elvis.alvarez.2001@gmail.com>
2025-10-03 22:49:57 -0400

    Configurando playwright config

---
+++ playwright.config.ts
@@ -0,0 +1,39 @@
+import { defineConfig, devices } from '@playwright/test';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(__dirname, '.env') });
+
+export default defineConfig({
+  testDir: './tests',
+  /* Run tests in files in parallel */
+  fullyParallel: true,
+  /* Fail the build on CI if you accidentally left test.only in the source code. */
+  forbidOnly: !!process.env.CI,
+  /* Retry on CI only */
+  retries: process.env.CI ? 2 : 0,
+  /* Opt out of parallel tests on CI. */
+  workers: process.env.CI ? 1 : undefined,
+  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
+  reporter: 'html',
+  
+  use: {
+    trace: 'on-first-retry',
+  },
+  projects: [
+    {
+      name: 'api-tests',
+      testMatch: 'tests/api/**/*.spec.ts',
+      use: {
+        baseURL: process.env.API_URL,
+      },
+    },
+    {
+      name: 'ui-tests',
+      testMatch: 'tests/ui/**/*.spec.ts',
+      use: {
+        ...devices['Desktop Chrome'],
+        baseURL: process.env.BASE_URL_UI
+      },
+    },
+  ],
+});
